801b80e82de0afeeeecd696c6bb82816164d8b57
Oleks <oleksstasyev@gmail.com>
2020-12-12 16:20:35 -0500

    Working on module removal

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,4 +1,4 @@
 require('dotenv').config()
 module.exports = {
-    url: 'mongodb+srv://Oleks:PlasticBottle823@cluster0.ka0hr.mongodb.net/community?retryWrites=true&w=majority'
+    url: 'hello'
  }
--- server/config/gpt3.js
+++ server/config/gpt3.js
@@ -1,3 +1,3 @@
 module.exports = {
-    key: 'sk-UAI47DsbnY4MFn1awPMAKXrax0lN52PBxofs1w8x'
+    key: 'hi'
 }   
